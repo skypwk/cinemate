d12469eb2c1812f9f308218b44b48f95d3341686
skypwk <skypwk@hanmail.net>
2025-10-23 18:45:46 +0900

    Complete file

---
+++ src/components/Footer.js
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom";
+export const Footer = () => {
+  return (
+    <footer className="bg-white  shadow-sm m-4 dark:bg-gray-800">
+        <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2030 <Link to="/" className="hover:underline">Cinemate™</Link>. All Rights Reserved.
+        </span>
+        <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+            <li>
+                <a href="https://www.instagram.com" target="_blank" className="hover:underline me-4 md:me-6">Instagram</a>
+            </li>
+            <li>
+                <a href="https://www.linkedin.com" target="_blank" className="hover:underline me-4 md:me-6">LinkedIn</a>
+            </li>
+            <li>
+                <a href="https://www.youtube.com" target="_blank" className="hover:underline me-4 md:me-6">Youtube</a>
+            </li>
+            <li>
+                <a href="https://www.github.com" target="_blank" className="hover:underline">Github</a>
+            </li>
+        </ul>
+        </div>
+    </footer>
+  )
+}
---
+++ src/pages/Search.js
@@ -0,0 +1,33 @@
+import { useFetch } from "../hooks/useFetch";
+import { useTitle} from "../hooks/useTitle";
+import { Card } from "../components";
+import { useSearchParams } from 'react-router-dom';
+import { useEffect } from 'react';
+
+export const Search = ({apiPath}) => {
+  const [ searchParams] = useSearchParams();
+  const queryTerm = searchParams.get("q");
+  const { data: movies} = useFetch(apiPath, queryTerm);
+
+  const pageTitle = useTitle(`Search result for ${queryTerm}`)
+  // useEffect(()=>{
+  //   document.title = `Search result for ${queryTerm} / Cinemate`;
+  // });
+  
+
+
+  return (
+    <main>
+      <section>
+        <p className='text-3xl text-gray-700 dark:text-white'>{ movies.length === 0 ? `No result found for '${queryTerm}'` : `Result for '${queryTerm}'`}</p>
+      </section>
+      <section className='max-w-7xl mx-auto py-7'>
+        <div className='flex justify-start flex-wrap'>
+          {movies.map((movie)=>(
+            <Card key={movie.id} movie={movie} />
+          ))}
+        </div>
+      </section>
+    </main>
+  )
+}
